2ca1f5ce4c63b0e16be2b785a82a2c2878969adc
hayriye uylukcu <huylukcu@gmail.comgit config --global user.email huylukcu@gmail.comgit config --global user.name hayriye>
2022-12-22 20:49:08 -0600

    commit

--- src/main/java/atmproject/Account.java
+++ src/main/java/atmproject/Account.java
@@ -1,7 +1,58 @@
 package atmproject;
 
+import java.util.Scanner;
+
 public class Account {
     private int accountNumber;
     private int pinNumber;
+    private double checkingBalance;
+    private double savingBalance;
+    static double iWantToHave = 200;
 
+    Scanner input = new Scanner(System.in);
+
+    public int getAccountNumber() {
+        return accountNumber;
+    }
+
+    public void setAccountNumber(int accountNumber) {
+        this.accountNumber = accountNumber;
+    }
+
+    public int getPinNumber() {
+        return pinNumber;
+    }
+
+    public void setPinNumber(int pinNumber) {
+        this.pinNumber = pinNumber;
+    }
+
+    public double getCheckingBalance() {
+        return checkingBalance;
+    }
+
+    public void setCheckingBalance(double checkingBalance) {
+        this.checkingBalance = checkingBalance;
+    }
+
+    public double getSavingBalance() {
+        return savingBalance;
+    }
+
+    public void setSavingBalance(double savingBalance) {
+        this.savingBalance = savingBalance;
+    }
+
+    public Scanner getInput() {
+        return input;
+    }
+
+    public void setInput(Scanner input) {
+        this.input = input;
+    }
+
+    public static void main(String[] args) {
+        checkingBalance  = 1000;
+
+    }
 }
